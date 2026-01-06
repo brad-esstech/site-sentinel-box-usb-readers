adaa9e0dd8ba4e6df371366783185be2a52b624d
brad-esstech <bbarton@esstech.com.au>
2026-01-06 21:18:01 +0800

    Update get_access_list.rb

--- get_access_list.rb
+++ get_access_list.rb
@@ -3,7 +3,7 @@
 require_relative 'config.rb' # load site config
 require 'httparty'
 
-#api_url = "https://staging.sitesentinel.com/api/gates/checks/batch"
+#api_url = "https://staging.sitesentinel.com/api/gates/access_tokens"
 api_url = "https://www.sitesentinel.com.au/api/gates/access_tokens"
 
 response = HTTParty.get(api_url, {
